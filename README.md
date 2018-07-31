d4faec2639ba2543f4fbf019f923e72cc6e3954c
JungleM0nkey <37726736+JungleM0nkey@users.noreply.github.com>
2018-07-31 16:38:14 -0400

    Second commit
    bitlocker script added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Collection of scripts and useful tools I have created in order to solve a problem or a set of problems.
+Collection of scripts and useful tools I have created to help with a problem or just for fun.
 
 1. check_pc_usage.ps1 outputs CPU, RAM and Disk utiliation stats for a given computer name. Make sure WSMAN is enabled on the target system (enable-psremoting)
+
+2. enable_bitlocker.ps1 enables bitlocker, fetches the recovery key information and stores it in a location under $backup_location
---
+++ enable_bitlocker.ps1
@@ -0,0 +1,25 @@
+﻿Enable-Bitlocker -MountPoint c: -SkipHardwareTest -RecoveryPasswordProtector -UsedSpaceOnly
+$id = (Get-BitLockerVolume -MountPoint C).KeyProtector.keyprotectorid[1].trim('{','}')
+$recovery_key = (Get-BitLockerVolume -MountPoint C).KeyProtector.recoverypassword
+#insert backup location for the recovery key
+$backup_location = ""
+$text = @"
+BitLocker Drive Encryption recovery key 
+
+To verify that this is the correct recovery key, compare the start of the following identifier with the identifier value displayed on your PC.
+
+Identifier:
+
+	$id
+
+If the above identifier matches the one displayed by your PC, then use the following key to unlock your drive.
+
+Recovery Key:
+
+	$recovery_key
+
+If the above identifier doesn't match the one displayed by your PC, then this isn't the right key to unlock your drive.
+Try another recovery key, or refer to https://go.microsoft.com/fwlink/?LinkID=260589 for additional assistance.
+
+"@
+write-output $text > "$backup_location\$env:computername - Bitlocker Recovery Key $id.TXT"
