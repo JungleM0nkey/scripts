ab4543e5b0fd10d0a344c0cfb1648b96bf08056c
JungleM0nkey <37726736+JungleM0nkey@users.noreply.github.com>
2019-07-22 10:08:31 -0400

    Add files via upload

---
+++ enable_WOL.ps1
@@ -0,0 +1,11 @@
+﻿#Disable Fast Boot.
+#powercfg -h off
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t reg_dword /d 0 /f
+#Change power settings to allow Wake on Lan magic packet to wake the NIC.
+get-netadapter -name Ethernet | Enable-NetAdapterPowerManagement -WakeOnMagicPacket
+#Configure bios to allow Wake on Lan for NIC (DELL)
+$cctk_path = "C:\Program Files (x86)\Dell\Command Configure\X86_64\cctk.exe"
+&$cctk_path --setuppwd=itdirector --valsetuppwd=itdirector
+&$cctk_path --deepsleepctrl=disable --valsetuppwd=itdirector
+&$cctk_path --wakeonlan=enable  --valsetuppwd=itdirector
+&$cctk_path --setuppwd= --valsetuppwd=itdirector
