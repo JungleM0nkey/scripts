e2fc6008fe7ee4af238c99dcef998c67388d2b53
JungleM0nkey <37726736+JungleM0nkey@users.noreply.github.com>
2019-07-22 10:13:48 -0400

    Add files via upload

---
+++ uptime.ps1
@@ -0,0 +1,12 @@
+﻿Param([string]$computerName)
+
+
+$connection = test-connection -count 1 $computername -ErrorAction SilentlyContinue
+if ($connection){
+$lastboot = (Get-CimInstance -ClassName win32_operatingsystem -ComputerName $computerName -ErrorAction SilentlyContinue | select lastbootuptime).lastbootuptime 
+$currentdate = get-date
+$difference = new-timespan -start $lastboot -end $currentdate -ErrorAction SilentlyContinue
+write-host $computername.ToUpper() "uptime:" $difference.Days "Days" $difference.Hours "Hours" $difference.Minutes "Minutes" -ForegroundColor Yellow
+}else{
+write-host "Unable to establish connection with" $computername.ToUpper() -ForegroundColor Red
+}
