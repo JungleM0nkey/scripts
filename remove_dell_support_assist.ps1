f8bc1b8c10efc99e3f4c2225c16dc8f8d5317c77
JungleM0nkey <37726736+JungleM0nkey@users.noreply.github.com>
2019-07-22 10:17:08 -0400

    Add files via upload

---
+++ remove_dell_support_assist.ps1
@@ -0,0 +1,4 @@
+﻿$support_assist = Get-CimInstance -Classname Win32_Product | Where-Object Name -Match ‘Dell SupportAssist’
+if ($support_assist){
+Get-CimInstance -Classname Win32_Product | Where-Object Name -Match ‘Dell SupportAssist’ | Invoke-CimMethod -MethodName UnInstall
+}
